b17fb51337f18ab12a5f7a1359e4fdbe3f9bfa76
steepbike2526 <steep.bike2526@fastmail.com>
2026-01-20 17:46:12 -0500

    Fix version bumping

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -2,21 +2,25 @@ import { readFile, writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 
 const [, , bumpType = 'patch'] = process.argv;
+const packageFile = resolve('package.json');
+const lockFile = resolve('package-lock.json');
 const versionFile = resolve('src/lib/version.ts');
-const contents = await readFile(versionFile, 'utf8');
 
-const match = contents.match(/appVersion\s*=\s*'(?<version>\d+\.\d+\.\d+)'/);
-if (!match?.groups?.version) {
-  throw new Error(`Could not find appVersion in ${versionFile}.`);
+const packageContents = await readFile(packageFile, 'utf8');
+const packageJson = JSON.parse(packageContents);
+const currentVersion = packageJson.version;
+
+if (!currentVersion) {
+  throw new Error(`Could not find version in ${packageFile}.`);
 }
 
-const [majorStr, minorStr, patchStr] = match.groups.version.split('.');
+const [majorStr, minorStr, patchStr] = currentVersion.split('.');
 let major = Number(majorStr);
 let minor = Number(minorStr);
 let patch = Number(patchStr);
 
 if ([major, minor, patch].some((value) => Number.isNaN(value))) {
-  throw new Error(`Invalid version found in ${versionFile}.`);
+  throw new Error(`Invalid version found in ${packageFile}.`);
 }
 
 switch (bumpType) {
@@ -37,6 +41,23 @@ switch (bumpType) {
 }
 
 const nextVersion = `${major}.${minor}.${patch}`;
-const nextContents = contents.replace(match.groups.version, nextVersion);
+packageJson.version = nextVersion;
+await writeFile(packageFile, `${JSON.stringify(packageJson, null, 2)}\n`);
+
+const lockContents = await readFile(lockFile, 'utf8');
+const lockJson = JSON.parse(lockContents);
+lockJson.version = nextVersion;
+if (lockJson.packages && lockJson.packages['']) {
+  lockJson.packages[''].version = nextVersion;
+}
+await writeFile(lockFile, `${JSON.stringify(lockJson, null, 2)}\n`);
+
+const versionContents = await readFile(versionFile, 'utf8');
+const versionMatch = versionContents.match(/appVersion\s*=\s*'(?<version>\d+\.\d+\.\d+)'/);
+if (!versionMatch?.groups?.version) {
+  throw new Error(`Could not find appVersion in ${versionFile}.`);
+}
+const nextContents = versionContents.replace(versionMatch.groups.version, nextVersion);
 await writeFile(versionFile, nextContents);
-console.log(`Updated appVersion to ${nextVersion}`);
+
+console.log(`Updated version to ${nextVersion}`);
